a36a82c668f2adf6f64dc0194530cb0b323b8c23
ishibustim <randyehret@gmail.com>
2015-07-18 12:40:47 -0500

    Moving drawing of navbar from server-side to client-side

--- index.php
+++ index.php
@@ -8,44 +8,50 @@
     <?php
         // Include backend modules
         include 'modules/apiModules/apiImports.php';
-
-        // Draw navbar
-        include 'modules/uiModules/navbar.php';
-
     ?>
 
-    <div id="main">
-
-        <div id="preSignIn">
-            <div id="signIn">
-                <div><div class="label">Username:</div><input name="username" type="text" /></div>
-                <div><div class="label">Password:</div><input name="password" type="password" /></div>
-                <div class="hidden"><div class="label">Verify Password:</div><input name="verifyPassword" type="password" /></div>
-                <div class="buttons">
-                    <input type="submit" value="Log In" id="signIn_logIn" />
-                    <input class="hidden" type="submit" value="Cancel" id="signIn_register_cancel" />
-                    <input type="submit" value="Register" id="signIn_register" />
-                </div>
+    <div id="preSignIn">
+        <div id="signIn">
+            <div><div class="label">Username:</div><input name="username" type="text" /></div>
+            <div><div class="label">Password:</div><input name="password" type="password" /></div>
+            <div class="hidden"><div class="label">Verify Password:</div><input name="verifyPassword" type="password" /></div>
+            <div class="buttons">
+                <input type="submit" value="Log In" id="signIn_logIn" />
+                <input class="hidden" type="submit" value="Cancel" id="signIn_register_cancel" />
+                <input type="submit" value="Register" id="signIn_register" />
             </div>
         </div>
-
-        <div id="postSignIn" class="hidden">
-            <div id="userInfo">
-                <span id="username"></span>
-                <i id="openSettings" class="fa fa-cog"></i>
-                <i id="logOut" class="fa fa-sign-out"></i>
-            </div>
-
-            <div id="newApiWindow">
-                <div><div class="label">Key ID:</div><input type="text" /></div>
-                <div><div class="label">Verification Code:</div><input type="text" /></div>
-                <div class="buttons">
-                    <input type="submit" value="&#10007; Cancel" id="newApiWindow_cancel" />
-                    <input type="submit" value="&#10003; Submit" id="newApiWindow_submit" />
-                </div>
-            </div>
-        </div>
-
     </div>
-    </body>
+
+    <div id="postSignIn" class="hidden">
+        <!-- Navbar -->
+        <div id='navbar'>
+            <div id='chars'>
+                <div class='charItem' id='startNew'><div class='charNew'>+</div>Add New Character API</div>
+            </div>
+            <div id='serverTime'>00:00</div>
+        </div>\n";
+
+        <!-- User Info Panel -->
+        <div id="userInfo">
+            <span id="username"></span>
+            <i id="openSettings" class="fa fa-cog"></i>
+            <i id="logOut" class="fa fa-sign-out"></i>
+        </div>
+
+        <!-- Add API Window -->
+        <div id="newApiWindow">
+            <div><div class="label">Key ID:</div><input type="text" /></div>
+            <div><div class="label">Verification Code:</div><input type="text" /></div>
+            <div class="buttons">
+                <input type="submit" value="&#10007; Cancel" id="newApiWindow_cancel" />
+                <input type="submit" value="&#10003; Submit" id="newApiWindow_submit" />
+            </div>
+        </div>
+
+        <!-- Main Content -->
+        <div id="main">
+        </div>
+    </div>
+</body>
 </html>
