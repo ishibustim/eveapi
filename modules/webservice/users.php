55ec90c17629753b4a2ec3d225d0bed77099fd95
ishibustim <randyehret@gmail.com>
2015-07-18 13:34:18 -0500

    Again reverting to in-line db connection

--- modules/webservice/users.php
+++ modules/webservice/users.php
@@ -4,10 +4,26 @@ include '../sql_login.php';
 
 if(isset($_POST['action']))
 {
-    $conn;
-    $db_selected;
+    //$conn;
+    //$db_selected;
 
-    connectToDatabase($sql_hostname, $sql_username, $sql_password, $sql_database_name, $conn, $db_selected);
+    //connectToDatabase($sql_hostname, $sql_username, $sql_password, $sql_database_name, $conn, $db_selected);
+
+    // Reverting to in-line db connection for the time being
+    $conn = mysql_connect($sql_hostname, $sql_username, $sql_password);
+
+    if(!$conn)
+    {
+        die("Could not connect: " . mysql_error());
+    }//end if
+
+    $db_selected = mysql_select_db($sql_database_name, $conn)
+        or die("Could not connect: " . mysql_error());
+
+    if(!$db_selected)
+    {
+        die("Could not use database: " . mysql_error());
+    }//end if
 
     switch($_POST['action'])
     {
