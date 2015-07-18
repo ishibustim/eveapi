38ccf960c38616db9e0a606829c5d0c501ec101f
ishibustim <randyehret@gmail.com>
2015-07-18 01:49:01 -0500

    Link registration form with users.php account creation

--- js/signIn.js
+++ js/signIn.js
@@ -1,5 +1,7 @@
 ﻿var registering = false;
 
+var usersPostURL = 'https://dev.ishibustim.homenet.org/passport/modules/webservice/users.php';
+
 function signIn_addEventListeners()
 {
     $('#signIn_register').click(beginRegister);
@@ -15,8 +17,31 @@ function beginRegister()
         $('#signIn_register_cancel').removeClass('hidden');
     }//end if
     else {
-        // register with webservice
-    }
+        var username = $('input[name="username"]').val();
+        var password = $('input[name="password"]').val();
+        var verifyPassword = $('input[name="verifyPassword"]').val();
+
+        if (password == verifyPassword && password.length >= 8) {
+            // register with webservice
+            $.post(usersPostURL, {
+                action: 'newUser',
+                username: username,
+                password: password,
+                verifyPassword: verifyPassword
+            }, function (data, status, xhr) {
+                if (status == 'success')
+                    alert('Account Created');
+                else
+                    alert('Could not create account');
+            });//end $.post
+        }//end if
+        else if(password != verifyPassword) {
+            alert('Passwords must match');
+        }//end else if password match
+        else if (password.length < 8) {
+            alert('Password must be longer than 8 characters');
+        }//end else if password length
+    }//end else (submit newUser request)
 }//end beginRegister
 
 function cancelRegister()
