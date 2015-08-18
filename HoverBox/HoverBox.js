0912da5407b109eda35db471ae7dc8ed36ad85c8
ishibustim <randyehret@gmail.com>
2015-08-18 17:50:16 -0500

    Update HoverBox

--- HoverBox/HoverBox.js
+++ HoverBox/HoverBox.js
@@ -14,47 +14,40 @@ function HoverBoxObj() {
 
   me.tooltip = null;
 
-  
-
-  HoverBoxObj.prototype.load = function() {
-    console.log('HoverBox: Loading...');  
-
-    // create HoverBox element
-    $('body').append('<div id="' + me.tooltipID + '"><div class="' + me.tooltipHeaderClass + '"></div><div class="' + me.tooltipBodyClass + '"></div></div>');
-    me.tooltip = $('#' + me.tooltipID);
+  // create HoverBox element
+  $('body').append('<div id="' + me.tooltipID + '"><div class="' + me.tooltipHeaderClass + '"></div><div class="' + me.tooltipBodyClass + '"></div></div>');
+  me.tooltip = $('#' + me.tooltipID);
 
 
-    // When the mouse enters an element with .HasTooltip, set the content of
-    // the tooltip and display it. Finally, start watching mouse movement on
-    // the entire document so the tooltip moves with the mouse.
-    $(me.hasTooltipClass).mouseenter(function() {
-      var titleText = $(this).attr(HoverBox.dataTitle);
-      var bodyText = $(this).attr(HoverBox.dataBody);
-      var bodyElem = $($(this).attr(HoverBox.dataBodyElem)).clone();
-      
-      // make sure that at least the title or body has been set
-      if((titleText && titleText.trim()) ||
-         (bodyText && bodyText.trim()) ||
-         bodyElem.length)
-      {
-        HoverBox.tooltip.children('.' + HoverBox.tooltipHeaderClass).append(titleText);
-        HoverBox.tooltip.children('.' + HoverBox.tooltipBodyClass).append(bodyText);
-        HoverBox.tooltip.children('.' + HoverBox.tooltipBodyClass).append(bodyElem);
-        HoverBox.tooltip.css({'display': 'block'});
-        $(document).mousemove(HoverBox.DisplayHoverbox);
-      }//end if
-    });
+  // When the mouse enters an element with .HasTooltip, set the content of
+  // the tooltip and display it. Finally, start watching mouse movement on
+  // the entire document so the tooltip moves with the mouse.
+  $(me.hasTooltipClass).mouseenter(function() {
+    var titleText = $(this).attr(HoverBox.dataTitle);
+    var bodyText = $(this).attr(HoverBox.dataBody);
+    var bodyElem = $($(this).attr(HoverBox.dataBodyElem)).clone();
+    
+    // make sure that at least the title or body has been set
+    if((titleText && titleText.trim()) ||
+       (bodyText && bodyText.trim()) ||
+       bodyElem.length)
+    {
+      HoverBox.tooltip.children('.' + HoverBox.tooltipHeaderClass).append(titleText);
+      HoverBox.tooltip.children('.' + HoverBox.tooltipBodyClass).append(bodyText);
+      HoverBox.tooltip.children('.' + HoverBox.tooltipBodyClass).append(bodyElem);
+      HoverBox.tooltip.css({'display': 'block'});
+      $(document).mousemove(HoverBox.DisplayHoverbox);
+    }//end if
+  });
 
-    // Empty the tooltip and hide it when the mouse leaves an element with
-    // .HasTooltip. Also, stop watching for mouse movement.
-    $(me.hasTooltipClass).mouseout(function() {
-      HoverBox.tooltip.children('.' + HoverBox.tooltipHeaderClass).empty();
-      HoverBox.tooltip.children('.' + HoverBox.tooltipBodyClass).empty();
-      HoverBox.tooltip.css({'display': 'none'});
-      $(document).off('mousemove', HoverBox.DisplayHoverbox);
-    });
-    console.log('HoverBox: Done');
-  };//end HoverBox.load
+  // Empty the tooltip and hide it when the mouse leaves an element with
+  // .HasTooltip. Also, stop watching for mouse movement.
+  $(me.hasTooltipClass).mouseout(function() {
+    HoverBox.tooltip.children('.' + HoverBox.tooltipHeaderClass).empty();
+    HoverBox.tooltip.children('.' + HoverBox.tooltipBodyClass).empty();
+    HoverBox.tooltip.css({'display': 'none'});
+    $(document).off('mousemove', HoverBox.DisplayHoverbox);
+  });
 
   // sets the position of the tooltip based on the location of the pointer
   HoverBoxObj.prototype.DisplayHoverbox = function(ev) {
